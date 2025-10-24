1dc9f46ddc244eefc45bea6ddd98b832b8819d50
paxelotl <141200190+paxelotl@users.noreply.github.com>
2025-10-24 13:21:44 -0700

    Lerp camera position

--- objects/player/mouse_and_look.gd
+++ objects/player/mouse_and_look.gd
@@ -3,15 +3,26 @@ extends Node3D
 @onready var camera_pivot = $CameraPivot
 @onready var mouse_on_window: bool = true
 
+@onready var initial_camera_distance: float = $CameraPivot/Camera.position.z
+var target_camera_distance: float
+var target_camera_position: Vector3
+
 func _ready() -> void:
+	target_camera_position = camera_pivot.global_position
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("alt"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	# mouse must be on window to be captured, otherwise error
 	if Input.is_action_just_released("alt") and mouse_on_window:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	_lerp_camera_position(delta)
+
+func _lerp_camera_position(delta: float):
+	target_camera_position = target_camera_position.lerp(global_position, delta * 13.0)
+	camera_pivot.global_position = target_camera_position
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
